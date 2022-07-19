3228a8c6058df9fbd0df4e12c63d3c84a04e696b
J. Nick Koston <nick@koston.org>
2022-07-19 11:24:05 -0500

    feat: init repo

--- src/sensorpush_ble/__init__.py
+++ src/sensorpush_ble/__init__.py
@@ -1,1 +1,8 @@
+"""Parser for SensorPush BLE advertisements."""
+from __future__ import annotations
+
+from .parser import SensorPushBluetoothDeviceData
+
 __version__ = "0.1.0"
+
+__all__ = ["SensorPushBluetoothDeviceData"]
---
+++ tests/test_parser.py
@@ -0,0 +1,5 @@
+from sensorpush_ble.parser import SensorPushBluetoothDeviceData
+
+
+def test_can_create():
+    SensorPushBluetoothDeviceData()
